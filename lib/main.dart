2dff6540d326f6cdf50b9b2e46ade05f3124542e
Jonathan <jonathanwilliamfirebase@gmail.com>
2020-04-11 12:27:33 -0300

    Jonathan William

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:uber/telas/Home.dart';
+import 'Rotas.dart';
 
 final ThemeData temaPadrao = ThemeData(
   primaryColor: Color(0xff37474f),
@@ -10,6 +11,8 @@ void main() => runApp(MaterialApp(
   title: "Uber",
   home: Home(),
   theme: temaPadrao,
+  initialRoute: "/",
+  onGenerateRoute: Rotas.gerarRotas,
   debugShowCheckedModeBanner: false,
 ));
 
