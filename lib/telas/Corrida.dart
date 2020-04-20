a9cfae4b12ce824aedf4a7c77cedc156adbff333
Jonathan <jonathanwilliamfirebase@gmail.com>
2020-04-20 10:16:21 -0300

    Jonathan William

--- lib/telas/Corrida.dart
+++ lib/telas/Corrida.dart
@@ -1,16 +1,130 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:uber/util/StatusRequisicao.dart';
 
 class Corrida extends StatefulWidget {
+
+  String idRequisicao;
+
+  Corrida(this.idRequisicao);
+
   @override
   _CorridaState createState() => _CorridaState();
 }
 
 class _CorridaState extends State<Corrida> {
 
-  
+  Completer<GoogleMapController> _controller = Completer();
+  CameraPosition _posicaoCamera = CameraPosition(
+      target: LatLng(-23.563999, -46.653256)
+  );
+  Set<Marker> _marcadores = {};
+
+  //controles para exibição na tela
+  String _textoBotao = "Aceitar corrida";
+  Color _corBotao = Color(0xff1ebbd8);
+  Function _funcaoBotao;
+
+  _alterarBotaoPrincipal(String texto, Color cor, Function funcao){
+
+    setState(() {
+      _textoBotao = texto;
+      _corBotao = cor;
+      _funcaoBotao = funcao;
+    });
+
+  }
+
+  _onMapCreated(GoogleMapController controller){
+
+    _controller.complete(controller);
+
+  }
+
+  _adicionarListenerLocalizacao(){
+
+    var geolocator = Geolocator();
+    var locationoptions = LocationOptions(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 10
+    );
+
+    geolocator.getPositionStream(locationoptions).listen((Position position){
+
+      _exibeMarcadorPassageiro(position);
+
+      _posicaoCamera = CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 19
+      );
+      _movimentarCamera(_posicaoCamera);
+    });
+
+  }
+
+  _recuperarUltimaLocalicaoConhecida() async{
+
+    Position position = await Geolocator()
+        .getLastKnownPosition(desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      if(position != null){
+
+        _exibeMarcadorPassageiro(position);
+
+        _posicaoCamera = CameraPosition(
+            target: LatLng(position.latitude, position.longitude),
+            zoom: 19
+        );
+
+        _movimentarCamera(_posicaoCamera);
+
+      }
+    });
+
+  }
+
+  _movimentarCamera(CameraPosition cameraPosition) async{
+
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+            cameraPosition
+        )
+    );
+
+  }
+
+  _exibeMarcadorPassageiro(Position local) async{
+
+    double pixelRatio = MediaQuery.of(context).devicePixelRatio;
+
+    BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(devicePixelRatio: pixelRatio),
+        "image/motorista.png"
+    ).then((BitmapDescriptor icone){
+
+      Marker marcadorPassageiro = Marker(
+          markerId: MarkerId("marcador-motorista"),
+          position: LatLng(local.latitude, local.longitude),
+          infoWindow: InfoWindow(
+              title: "Meu local"
+          ),
+          icon: icone
+      );
+
+      setState(() {
+        _marcadores.add(marcadorPassageiro);
+      });
+
+    });
+
+  }
 
   @override
   void initState() {
@@ -24,6 +138,43 @@ class _CorridaState extends State<Corrida> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Painel corrida"),
+      ),
+      body: Container(
+        child: Stack(
+          children: <Widget>[
+            GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: _posicaoCamera,
+              onMapCreated: _onMapCreated,
+              //myLocationEnabled: true,
+              myLocationButtonEnabled: false,
+              markers: _marcadores,
+            ),
+            Positioned(
+                right: 0,
+                left: 0,
+                bottom: 0,
+                child: Padding(
+                  padding: Platform.isIOS
+                      ? EdgeInsets.fromLTRB(20, 10, 20, 25)
+                      : EdgeInsets.all(10),
+                  child: RaisedButton(
+                    child: Text(
+                      _textoBotao,
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    color: _corBotao,
+                    padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    onPressed: _funcaoBotao,
+                  ),
+                )
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
